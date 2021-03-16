57986e55e9e4757dad55628bc8ce888f773b625c
varshinisensak <80758131+varshinisensak@users.noreply.github.com>
2021-03-16 17:39:41 +0530

    Add files via upload

--- task1.js
+++ task1.js
@@ -1,1 +1,29 @@
+// returns number
+var num=1998;
+console.log(typeof(num));
 
+//returns string
+var a="varshini";
+console.log(typeof(a));
+
+//return object
+var a={firstName:"varshini", lastName:"surya"};
+console.log(typeof(a));
+
+//returns boolean
+var a=100;
+var b=500;
+console.log(a==b);
+console.log(a!==b);
+
+//array
+var student=["varsh","surya","guru","vaishu"];
+console.log(typeof(student));
+
+//null
+var a=null;
+console.log(typeof(a));
+
+//undefined
+var x;
+console.log(typeof(x));
