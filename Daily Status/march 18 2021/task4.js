59de04cc0ce8d292e4087e722f2f231129208b70
varshinisensak <80758131+varshinisensak@users.noreply.github.com>
2021-03-22 14:26:15 +0530

    Add files via upload

--- Daily Status/march 18 2021/task4.js
+++ Daily Status/march 18 2021/task4.js
@@ -1,1 +1,27 @@
+const bike =
+    {
+    model:2,
+    name:"ford",
+    wheel:4,
+    color:"red"
+    }
+console.log(bike.name);
 
+
+
+
+
+
+
+const bike =
+{
+    
+        model:2,
+        name:"ford",
+        wheel:4,
+        color:"red",
+        result: function () {
+        console.log(this.name);
+        }
+        };
+      bike.result();
